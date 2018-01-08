aac305f249389ab3ca16927620cd3f0d89b912bd
avanegasp <arco527@gmail.com>
2018-01-08 16:35:46 -0500

    Challenge done

---
+++ solution.rb
@@ -0,0 +1,19 @@
+# require 'sinatra'
+#   get '/' do
+#   if request.env["HTTP_PERMISO"] == "soy-un-token-secreto."
+#     "Si lo logramos"
+#   else
+#     "Sin permiso"
+#   end
+# end
+
+
+require 'sinatra'
+get '/' do
+  if request.env["HTTP_PERMISO"] == "soy-un-token-secreto"
+    "Si lo logramos!"
+  else
+    "Sin Permiso"
+  end
+
+end
